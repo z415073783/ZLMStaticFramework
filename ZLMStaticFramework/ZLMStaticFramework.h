a1d88d7cafd6922c64913aa553f010cda6b56c14
z415073783 <415073783@qq.com>
2015-04-03 09:54:30 +0800

    1
    1

---
+++ ZLMStaticFramework/ZLMStaticFramework.h
@@ -0,0 +1,34 @@
+//
+//  ZLMStaticFramework.h
+//  ZLMStaticFramework
+//
+//  Created by Zlm on 15-3-23.
+//  Copyright (c) 2015年 zlm. All rights reserved.
+//
+#ifndef ZLMStaticFramework_ZLMStaticFramework_h
+#define ZLMStaticFramework_ZLMStaticFramework_h
+#import "AdaptiveDeviceManage.h" //适配策略
+#import "BaseViewController.h"  //基础类
+#import "UIButton+BaseButton.h"  //UIButton扩展
+#import "UILabel+BaseLabel.h"  //Label扩展
+#import "UITextView+BaseTextView.h" //TextView扩展
+#import "SharedManage.h"  //打开新界面
+#import "WholeDefine.h"
+#import "NetHttpManage.h"  //网络请求
+#import "SVProgressHUD.h"  //风火轮
+
+
+/*
+ 如下类需手动引入:
+ CalendarManage.h  日历
+ PhotoManageC.h   相册管理
+ LocalFileManage.h  本地文件管理
+ EGORefreshTableHeaderView.h  下拉刷新
+ UIImageView+WebCache.h  网络图片加载
+ 
+ 
+ 
+ */
+
+
+#endif
